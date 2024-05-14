19fd615d9106e9fba11765a8e7c8deff9799b9f6
Saile <sailesantos0@gmail.com>
2024-05-14 01:38:01 -0400

    disabled se quantidade igual a 0

--- src/app/components/ListagemCarrinhoRow/ItemCarrinhoRow.tsx
+++ src/app/components/ListagemCarrinhoRow/ItemCarrinhoRow.tsx
@@ -21,7 +21,8 @@ const ItemCarrinhoRow = (
             <td>{item.nome}</td>
             <td>R$ {(item.preco).toFixed(2)}</td>
             <td>
-                <button className="btn btn-secondary btn-sm me-2" onClick={() => diminuiQuantidade(item.id)}>-</button>
+                
+                <button className="btn btn-secondary btn-sm me-2" onClick={() => diminuiQuantidade(item.id)}  disabled={item.quantidade == 0}>-</button>
                 {item.quantidade}
                 <button className="btn btn-secondary btn-sm ms-2"onClick={() => aumentaQuantiade(item.id)}>+</button>
             </td>
